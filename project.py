7409eab53aa8b7da1caa05c5bc20e6c586137774
SJaa2610 <sjapoloa@uce.edu.ec>
2024-11-13 20:16:49 -0500

    Se agregaron cambios

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,5 +8,5 @@ def hello():
     return "Hi, I'm Sarahi. This is my Python project"
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)  # Asegura que Flask escuche en 0.0.0.0 y en el puerto 8080
-    
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", default=8080)))  # Asegura que Flask escuche en 0.0.0.0 y en el puerto 8080
+
